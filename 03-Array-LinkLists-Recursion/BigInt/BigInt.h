eed52d133be04ea57f7a9962f0b56db1c49f5126
mtmattqq <mtmattqq@gmail.com>
2024-02-09 11:51:04 +0800

    BigInt constructor

---
+++ 03-Array-LinkLists-Recursion/BigInt/BigInt.h
@@ -0,0 +1,61 @@
+#ifndef BIGINT_H
+#define BIGINT_H
+
+#include <iostream>
+#include "../MyVector/MyVector.h"
+#include <cstdint>
+#include <string>
+#include <stdexcept>
+
+class BigInt {
+public:
+    BigInt();
+    BigInt(std::string);
+    BigInt(int64_t);
+    BigInt(BigInt&);
+
+    friend std::ostream& operator<<(std::ostream &out, BigInt&);
+    friend std::istream& operator>>(std::istream &in, BigInt&);
+private:
+    MyVector<int16_t> number;
+    int sign;
+};
+
+BigInt::BigInt()
+    : number(1), sign(0) {}
+
+BigInt::BigInt(std::string num) {
+    if(num[0] == '-') {
+        sign = -1;
+        num.erase(0, 1);
+    } else {
+        sign = 1;
+    }
+    number.resize(num.size());
+    for(int i{num.size() - 1}; i >= 0; --i) {
+        if(!isdigit(num[i])) {
+            throw std::range_error("Error: found non digit char in the string. (BigInt)\n");
+        }
+        number[i] = num[i] - '0';
+    }
+}
+
+BigInt::BigInt(int64_t num) {
+    if(num < 0) {
+        sign = -1;
+        num = -num;
+    } else {
+        sign = 1;
+    }
+    while(num > 0) {
+        number.push_back(num % 10);
+        num /= 10;
+    }
+}
+
+BigInt::BigInt(BigInt &num) {
+    sign = num.sign;
+    number = num.number;
+}
+
+#endif
